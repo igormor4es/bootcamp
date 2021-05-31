b2e80356190bf76c69fbcdd766946b311fb1e252
Igor de Moraes Silva Ribeiro <igormoraes90@yahoo.com.br>
2021-05-31 15:54:37 -0300

    Adicionando anotações.

--- src/main/java/com/project/bootcamp/model/dto/StockDTO.java
+++ src/main/java/com/project/bootcamp/model/dto/StockDTO.java
@@ -1,13 +1,29 @@
 package com.project.bootcamp.model.dto;
 
 import java.time.LocalDate;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class StockDTO {
     
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    @DecimalMin(value = "0.00")
+    @Digits( integer = 6, fraction = 2)
     private Double price;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
+
+    @NotNull
+    @Digits( integer = 3, fraction = 2)
     private Double variation;
 
     public Long getId() {
@@ -50,5 +66,4 @@ public class StockDTO {
         this.variation = variation;
     }
 
-    
 }
